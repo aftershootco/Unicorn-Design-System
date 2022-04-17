3354ee2daa05929ef284bf29c07e7ad4e9e357ac
frannkenstein <deathakanote@gmail.com>
2022-04-17 08:44:44 +0530

    Revert "build added"
    This reverts commit 3e05ef47922421af35a387820ecafdf03ba01839.

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,40 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
-import dts from 'rollup-plugin-dts'
-import postcss from 'rollup-plugin-postcss'
-import svg from 'rollup-plugin-svg'
-const packageJson = require('./package.json')
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+
+const packageJson = require("./package.json");
 
 export default [
-	{
-		input: 'src/components/index.ts',
-		output: [
-			{
-				file: packageJson.main,
-				format: 'cjs',
-				sourcemap: true,
-			},
-			{
-				file: packageJson.module,
-				format: 'esm',
-				sourcemap: true,
-			},
-		],
-		plugins: [
-			resolve(),
-			commonjs(),
-			postcss({
-				extract: true,
-				use: ['sass'],
-			}),
-			svg(),
-			typescript({ tsconfig: './tsconfig.json' }),
-		],
-	},
-	{
-		input: 'dist/esm/types/index.d.ts',
-		output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-		plugins: [dts()],
-		external: [/\.scss$/],
-	},
-]
+  {
+    input: "src/components/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      postcss({
+        extract: true,
+        use: ["sass"],
+      }),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.scss$/],
+  },
+];
