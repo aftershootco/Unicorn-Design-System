788886411d359afd4d618f4fafa0e374ec13f530
Shaney Waris <shaney18308@iiitd.ac.in>
2022-07-17 17:45:00 +0530

    stroke-linejoin to strokeLinejoin and stroke-linecap to strokeLinecap

--- src/components/SVG/CheckMark.tsx
+++ src/components/SVG/CheckMark.tsx
@@ -7,7 +7,7 @@ interface SVGProps {
 const DownArrow: React.FC<SVGProps> = ({ className }: SVGProps) => {
 	return (
 		<svg width='12' height='9' viewBox='0 0 12 9' fill='none' xmlns='http://www.w3.org/2000/svg'>
-			<path d='M11 1L4.125 8L1 4.81818' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' />
+			<path d='M11 1L4.125 8L1 4.81818' stroke='white' stroke-width='1.5' strokeLinecap='round' strokeLinejoin='round' />
 		</svg>
 	)
 }
