0c466854929f160cbf2c09d09a53e2f31474db68
Shaney Waris <shaney18308@iiitd.ac.in>
2022-07-18 12:40:25 +0530

    [PreferenceButton] minor bug fixed, Storybook working fine now

--- src/components/PreferenceButton/PreferenceButton.tsx
+++ src/components/PreferenceButton/PreferenceButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import clsx from 'clsx'
-import './prefer.scss'
+import './Prefer.scss'
 export interface PreferenceButtonProps {
 	children?: string | React.ReactNode
 	className?: string
