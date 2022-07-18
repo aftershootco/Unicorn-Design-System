c3b378397059ef18cb4de8a31481eb4c2bb873dd
Shaney Waris <shaney18308@iiitd.ac.in>
2022-07-18 13:29:56 +0530

    Toggle Btn story added

---
+++ src/stories/Toggle.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+import { Toggle, ToggleProps } from '../components'
+
+export default {
+	title: 'component/Toggle',
+	component: Toggle,
+} as Meta
+
+const Template: Story<ToggleProps> = (args: ToggleProps) => <Toggle {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+	value: false,
+	onClick: () => {},
+    options: {
+        first: 'First',
+        second: 'Second',
+    }
+}
