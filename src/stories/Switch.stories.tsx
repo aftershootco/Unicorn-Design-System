ed9ad001a712fe046822f7c7daffdcbd440438c8
Shaney Waris <shaney18308@iiitd.ac.in>
2022-07-19 05:30:58 -0700

    Added docs and removed bugs (#3)
    * Unused code removed from Dropdown
    
    * removed bugs and added docs
    
    * Updated docs and code
    
    * [Chip] EOL added

--- src/stories/Switch.stories.tsx
+++ src/stories/Switch.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Meta, Story } from '@storybook/react'
-import { Switch,SwitchProps } from '../components'
+import { Switch, SwitchProps } from '../components'
 
 export default {
 	title: 'component/Switch',
@@ -11,5 +11,5 @@ const Template: Story<SwitchProps> = (args: SwitchProps) => <Switch {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-	defaultValue:false
+	value: false,
 }
