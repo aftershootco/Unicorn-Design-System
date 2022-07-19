e66fe62ae86bacce0531fcd042f4ac8a17d181b5
aniketxparihar <aniketxparihar@gmail.com>
2022-07-19 12:46:49 +0530

    Toggle done

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -13,15 +13,14 @@ export interface ToggleProps {
 const Toggle: React.FC<ToggleProps> = (props: ToggleProps) => {
 	return (
 		<div className='toggler flex-row' style={props.style}>
-			<button className='yearly text-h4-bold color-white cursor-pointer' onClick={() => props.onClick(true)}>
+			<button className="monthly tertiary text-h4-bold color-white cursor-pointer" onClick={() => props.onClick(false)}>
 				{props.options.first}
 			</button>
-			<button className='monthly two text-h4-bold color-white cursor-pointer' onClick={() => props.onClick(false)}>
+			<button className="yearly tertiary text-h4-bold color-white cursor-pointer" onClick={() => props.onClick(true)}>
 				{props.options.second}
 			</button>
-
-			<button className={'toggle_button text-h4-bold color-white' + (!props.value ? ' toggleButton' : '')}>
-				{props.value ? props.options.first : props.options.second}
+			<button className={'toggle_button text-h4-bold color-white' + (!props.value ? ' toggleButton ' : '')}> 
+			{!props.value ? props.options.second : props.options.first}
 			</button>
 		</div>
 	)
