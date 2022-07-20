86a5881d1bab22757ddeb8192c93a38c16c44232
aniketxparihar <aniketxparihar@gmail.com>
2022-07-20 16:11:55 +0530

    fix: toggle functionality , Transparent background , spacing

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -15,7 +15,7 @@ export interface ToggleProps {
 	/**
 	 * Function to be called when toggle is changed.
 	 */
-	onClick: (value: boolean) => void
+	onToggle: (value: boolean) => void
 
 	/**
 	 * Options name for the toggle.
@@ -29,13 +29,13 @@ export interface ToggleProps {
 const Toggle: React.FC<ToggleProps> = (props: ToggleProps) => {
 	return (
 		<div className='toggler flex-row' style={props.style}>
-			<button className="monthly tertiary text-h4-bold color-white cursor-pointer" onClick={() => props.onClick(false)}>
+			<button className="monthly button-tertiary text-h4-bold color-white cursor-pointer" onClick={()=>props.onToggle(!props.value)}>
 				{props.options.first}
 			</button>
-			<button className="yearly tertiary text-h4-bold color-white cursor-pointer" onClick={() => props.onClick(true)}>
+			<button className="yearly button-tertiary text-h4-bold color-white cursor-pointer"  onClick={()=>props.onToggle(!props.value)}>
 				{props.options.second}
 			</button>
-			<button className={'toggle_button text-h4-bold color-white' + (!props.value ? ' toggleButton ' : '')}> 
+			<button  className={'toggle_button text-h4-bold color-white' + (!props.value ? ' toggleButton ' : '')}> 
 			{!props.value ? props.options.second : props.options.first}
 			</button>
 		</div>
