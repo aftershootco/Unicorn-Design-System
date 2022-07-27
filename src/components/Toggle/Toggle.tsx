3df3c93ebac6426733e545808f5bdd5e8c99ff3b
Shaney Waris <shaney18308@iiitd.ac.in>
2022-07-27 14:19:30 +0530

    [Toggle Updated]

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -5,7 +5,7 @@ export interface ToggleProps {
 	/**
 	 * Styles to be applied on the toggle.
 	 */
-	style: React.CSSProperties
+	style?: React.CSSProperties
 
 	/**
 	 * If true, first option is selected. Otherwise the other one.
@@ -28,15 +28,16 @@ export interface ToggleProps {
 
 const Toggle: React.FC<ToggleProps> = (props: ToggleProps) => {
 	return (
-		<div className='toggler flex-row' style={props.style}>
-			<button className="monthly button-tertiary text-h4-bold color-white cursor-pointer" onClick={()=>props.onClick(!props.value)}>
+		<div className='toggler flex-row'>
+			<button className='yearly cursor-pointer text-h4-bold color-white' onClick={() => props.onClick(!props.value)}>
 				{props.options.first}
 			</button>
-			<button className="yearly button-tertiary text-h4-bold color-white cursor-pointer"  onClick={()=>props.onClick(!props.value)}>
+			<button className='monthly cursor-pointer two text-h4-bold color-white' onClick={() => props.onClick(!props.value)}>
 				{props.options.second}
 			</button>
-			<button  className={'toggle_button text-h4-bold color-white' + (!props.value ? ' toggleButton ' : '')}> 
-			{!props.value ? props.options.second : props.options.first}
+
+			<button className={'toggle_button text-h4-bold color-white cursor-pointer' + (!props.value ? ' toggleButton' : '')}>
+				{!props.value ? props.options.second : props.options.first}
 			</button>
 		</div>
 	)
