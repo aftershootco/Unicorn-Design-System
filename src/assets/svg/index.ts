999e494f36f6ad29f5d4e8f4effe2d5bfaf1f79a
Yash Johri <yashjohri1200@gmail.com>
2022-07-27 13:29:38 +0530

    [SVG] Remove faltu ka code

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,6 +1,5 @@
 import EyeOff from './EyeOff.svg'
 import EyeOn from './EyeOn.svg'
-import DownArrow from './DownArrow.svg'
 import Check from './Check.svg'
 
-export { EyeOff, EyeOn, DownArrow, Check }
+export { EyeOff, EyeOn, Check }
