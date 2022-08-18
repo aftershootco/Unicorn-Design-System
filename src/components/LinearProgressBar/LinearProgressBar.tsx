61d2f316d63b0821b3f24b144eff544d55c3698b
Manish Doley <manishdoley23@gmail.com>
2022-08-18 16:57:03 +0530

    update linear progress bar with classname format

--- src/components/LinearProgressBar/LinearProgressBar.tsx
+++ src/components/LinearProgressBar/LinearProgressBar.tsx
@@ -2,44 +2,38 @@ import React from 'react'
 import './LinearProgressBar.scss'
 
 export interface LinearProgressBarProps {
-	// set the value to change between determinant and indeterminant
-	// 0 for indeterminant
+	/**
+	 * set the value to change between determinant and indeterminant, 0 for indeterminant
+	 */
 	value?: number
 
-	// line color
-	lineBackgroundColor?: string
+	/**
+	 * background color of progress bar
+	 */
+	progressBarBackgroundColor?: string
 
-	// subline color
-	sublineBackgroundColor?: string
+	/**
+	 * color of progress bar
+	 */
+	progressBarColor?: string
 }
 
 const LinearProgressBar: React.FC<LinearProgressBarProps> = (props) => {
-
-	const lineStyle = {
-		background: `${props.lineBackgroundColor}`,
-	}
-
-	const sublineStyle = {
-		background: `${props.sublineBackgroundColor}`,
-	}
-
-	const sublineStyleDeter = {
-		background: `${props.sublineBackgroundColor}`,
-		width: props.value + '%',
-	}
-
 	return (
 		<>
-			{(props.value > 0) ? (
-				<div className='slider'>
-					<div className='line' style={lineStyle}></div>
-					<div className='subline' style={sublineStyleDeter}></div>
+			{props.value > 0 ? (
+				<div className={`slider relative w-100 m-4-t br-10 overflow-x-hidden`}>
+					<div className={`line absolute w-100 br-10 bg-43`} style={{ background: `${props.progressBarBackgroundColor}` }}></div>
+					<div
+						className={`subline absolute br-10 bg-teal100`}
+						style={{ background: `${props.progressBarColor}`, width: props.value + '%' }}
+					></div>
 				</div>
 			) : (
-				<div className='slider'>
-					<div className='line' style={lineStyle}></div>
-					<div className='subline inc' style={sublineStyle}></div>
-					<div className='subline dec' style={sublineStyle}></div>
+				<div className={`slider relative w-100 m-4-t br-10 overflow-x-hidden`}>
+					<div className={`line absolute w-100 br-10 bg-43`} style={{ background: `${props.progressBarBackgroundColor}` }}></div>
+					<div className={`subline inc absolute br-10 bg-teal100`} style={{ background: `${props.progressBarColor}` }}></div>
+					<div className={`subline dec absolute br-10 bg-teal100`} style={{ background: `${props.progressBarColor}` }}></div>
 				</div>
 			)}
 		</>
@@ -48,8 +42,8 @@ const LinearProgressBar: React.FC<LinearProgressBarProps> = (props) => {
 
 LinearProgressBar.defaultProps = {
 	value: 0,
-	lineBackgroundColor: '#434343',
-	sublineBackgroundColor: '#1da0bc'
+	progressBarBackgroundColor: '#434343',
+	progressBarColor: '#1da0bc',
 }
 
 export default LinearProgressBar
