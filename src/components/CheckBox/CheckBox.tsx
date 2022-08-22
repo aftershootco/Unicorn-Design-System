ae4a7716e157f3c11b23dfb6aff2f1f041dfe96b
Yash Johri <yashjohri1200@gmail.com>
2022-08-22 21:51:07 +0530

    [Tailwind] Some changes

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,47 +1,41 @@
-import React, { useCallback } from "react";
-import { classNames } from "../../utils";
-import Check from "../SVG/Check";
-// import './CheckBox.scss'
+import React, { useCallback } from 'react'
+import { classNames } from '../../utils'
+import Check from '../SVG/Check'
 
 export interface CheckBoxProps {
-  /**
-   * Classes to be applied to the button
-   */
-  className?: string;
-  /**
-   * Value of the checkbox.
-   * @Default false
-   */
-  value: boolean;
+	/**
+	 * Classes to be applied to the button
+	 */
+	className?: string
 
-  /**
-   * Function to be called when checkbox is checked.
-   */
-  onChange: (
-    value: boolean,
-    event?: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => void;
+	/**
+	 * Value of the checkbox.
+	 * @Default false
+	 */
+	value: boolean
+
+	/**
+	 * Function to be called when checkbox is checked.
+	 */
+	onChange: (value: boolean, event?: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
 }
 
 const CheckBox: React.FC<CheckBoxProps> = (props) => {
-  const onClick = useCallback(
-    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-      props.onChange(!props.value, e);
-    },
-    [props.onChange, props.value]
-  );
+	const onClick = useCallback(
+		(e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+			props.onChange(!props.value, e)
+		},
+		[props.onChange, props.value]
+	)
 
-  return (
-    <div
-      className={classNames(
-        "relative bg-black flex-col align-center justify-center cursor-pointer w-7 h-7",
-        props.className
-      )}
-      onClick={onClick}
-    >
-      {props.value && <Check className="w-6 text-black" />}
-    </div>
-  );
-};
+	return (
+		<div
+			className={classNames('relative bg-black flex-col align-center justify-center cursor-pointer w-7 h-7', props.className)}
+			onClick={onClick}
+		>
+			{props.value && <Check className='w-6 text-black' />}
+		</div>
+	)
+}
 
-export default React.memo(CheckBox);
+export default React.memo(CheckBox)
