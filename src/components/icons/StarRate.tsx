ab5f13914d4a77c533b4981bf59fdc4ea2d886af
frannkenstein <deathakanote@gmail.com>
2022-10-31 14:09:54 +0530

    [Icons] React memo corrected

--- src/components/icons/StarRate.tsx
+++ src/components/icons/StarRate.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const StarRate: React.FC<React.SVGProps<SVGSVGElement>> = React.memo((props) => {
+const StarRate: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
 	return (
 		<svg
 			xmlns='http://www.w3.org/2000/svg'
@@ -17,6 +17,6 @@ const StarRate: React.FC<React.SVGProps<SVGSVGElement>> = React.memo((props) => 
 			</g>
 		</svg>
 	)
-})
+}
 
-export default StarRate
+export default React.memo(StarRate)
