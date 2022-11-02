42957251a0a97e4b23208f4cc43c1b24bea2c72a
manis <manishdoley23@gmail.com>
2022-11-02 13:16:03 +0530

    [Slider] update slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -41,43 +41,10 @@ const Slider: React.FC<SliderProps> = (props) => {
 
 	return (
 		<>
-			<span className='MuiSlider-root MuiSlider-colorPrimary MuiSlider-sizeMedium css-141zskv'>
-				<span className='MuiSlider-rail css-b04pc9'></span>
-				<span style={{ left: '0%', width: `${props.value} + %` }} className='MuiSlider-track css-1t2bqnt'></span>
-				<span
-					data-index='0'
-					data-focusvisible='false'
-					className='MuiSlider-thumb MuiSlider-thumbColorPrimary MuiSlider-thumbSizeMedium css-19c0tnq'
-					style={{ left: '74%' }}
-				>
-					<input
-						data-index='0'
-						type='range'
-						min='0'
-						max='100'
-						step={props.step}
-						onChange={handleChange}
-						style={{
-							border: '0px',
-							clipPath: 'rect(0px, 0px, 0px, 0px)',
-							height: '100%',
-							margin: '-1px',
-							overflow: 'hidden',
-							padding: '0px',
-							position: 'absolute',
-							whiteSpace: 'nowrap',
-							width: '100%',
-							direction: 'ltr',
-						}}
-						value={props.value}
-					/>
-					<span className='MuiSlider-valueLabel css-3besu' aria-hidden='true'>
-						<span className='MuiSlider-valueLabelCircle'>
-							<span className='MuiSlider-valueLabelLabel'>{props.value}</span>
-						</span>
-					</span>
-				</span>
-			</span>
+			<div className='slider-container'>
+				<div className='slider-track' style={{ width: props.value }}></div>
+				<input className='slider-thumb' type='range' min='0' max='100' step={props.step} onChange={handleChange} value={props.value} />
+			</div>
 			{/* <input
 				className='slider-input'
 				type='range'
