a73a2ef5ccac72d3ccec1d848ddc82b653c572fb
frannkenstein <deathakanote@gmail.com>
2022-11-14 17:18:41 +0530

    storybook running

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/preset-scss'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/preset-scss', '@storybook/addon-postcss'],
 	framework: '@storybook/react',
 }
