c8456b3dae758a5c8a69d686c8ff053835aeeec2
Yash Johri <yashjohri1200@gmail.com>
2022-11-16 16:49:04 +0530

    [Accordion] Some more changes

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,7 +1,10 @@
 import { KeyBoardArrowRightIcon } from '@aftershootco/unicorn-icons'
-import React, { useCallback, useState } from 'react'
+import React from 'react'
 
 export interface AccordionProps {
+	expanded?: boolean
+	onChange?: (expanded: boolean) => void
+
 	header: string | JSX.Element
 	headerClassName?: string
 	headerStyle?: React.CSSProperties
@@ -9,27 +12,18 @@ export interface AccordionProps {
 	body: string | JSX.Element
 	bodyClassName?: string
 	bodyStyle?: React.CSSProperties
-
-	onClick?: VoidFunction
 }
 
 const Accordion: React.FC<AccordionProps> = React.forwardRef((props, ref: any) => {
-	const [expanded, setExpanded] = useState(false)
-
-	const onClick = useCallback(() => {
-		setExpanded((expanded) => !expanded)
-		props.onClick?.()
-	}, [expanded])
-
 	return (
-		<div className='w-full cursor-pointer rounded bg-gray-800 text-lg' onClick={onClick} ref={ref}>
+		<div className='w-full cursor-pointer rounded bg-gray-800 text-lg' onClick={() => props.onChange(!props.expanded)} ref={ref}>
 			<div className='flex w-full items-center p-3'>
-				<KeyBoardArrowRightIcon className={`text-gray-400 transition-all ${expanded ? 'rotate-90' : 'rotate-0'}`} />
+				<KeyBoardArrowRightIcon className={`text-gray-400 transition-all ${props.expanded ? 'rotate-90' : 'rotate-0'}`} />
 				<div className={`mx-2 text-ellipsis text-gray-50 ${props.headerClassName}`} style={props.headerStyle}>
 					{props.header}
 				</div>
 			</div>
-			{expanded && (
+			{props.expanded && (
 				<div className={`mx-9 w-full transform p-2 text-base text-gray-200 ${props.bodyClassName}`} style={props.bodyStyle}>
 					{props.body}
 				</div>
