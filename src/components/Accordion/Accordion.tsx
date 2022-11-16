7210be8af76b1ab06f58682437dd2d7852557af2
Yash Johri <yashjohri1200@gmail.com>
2022-11-16 16:29:39 +0530

    [Accordion] Some more changes

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -14,22 +14,22 @@ export interface AccordionProps {
 }
 
 const Accordion: React.FC<AccordionProps> = React.forwardRef((props, ref: any) => {
-	const [state, setState] = useState(false)
+	const [expanded, setExpanded] = useState(false)
 
 	const onClick = useCallback(() => {
-		setState((state) => !state)
-		props.onClick()
-	}, [state])
+		setExpanded((expanded) => !expanded)
+		props.onClick?.()
+	}, [expanded])
 
 	return (
 		<div className='w-full cursor-pointer rounded bg-gray-800 text-lg' onClick={onClick} ref={ref}>
 			<div className='align-center flex w-full p-3'>
-				<KeyBoardArrowRightIcon className={`text-gray-400 transition-all ${state ? 'rotate-90' : 'rotate-0'}`} />
+				<KeyBoardArrowRightIcon className={`text-gray-400 transition-all ${expanded ? 'rotate-90' : 'rotate-0'}`} />
 				<div className={`mx-2 text-ellipsis text-gray-50 ${props.headerClassName}`} style={props.headerStyle}>
 					{props.header}
 				</div>
 			</div>
-			{state && (
+			{expanded && (
 				<div className={`mx-9 w-full transform p-2 text-base text-gray-200 ${props.bodyClassName}`} style={props.bodyStyle}>
 					{props.body}
 				</div>
