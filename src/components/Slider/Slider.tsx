e2935a30543cd0a5f2486732c8c5ae9348438517
manis <manishdoley23@gmail.com>
2022-11-16 19:42:09 +0530

    [Slider] create slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import './Slider.scss';
 
-const Slider: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => {
+const Slider: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (
+	props
+) => {
 	return (
 		<>
-			<input {...props} type='range' className={'bg-red-900'} />
-			<div className='text-red-500'>HEllo</div>
+			<div
+				className={`h-[34px] w-full flex items-center justify-center rounded`}
+			>
+				<input
+					{...props}
+					disabled={props.disabled}
+					className={`w-4/5 rounded-sm bg-gradient-to-b ${
+						!props.disabled
+							? 'from-blue-600 to-blue-600'
+							: 'from-grey-1000 to-grey-1000 opacity-40'
+					} bg-no-repeat bg-grey-1000 slider-input ${props.className}`}
+					type='range'
+					style={{
+						...props.style,
+						backgroundSize: ((Number(props.value) - Number(props.min)) * Number(props.max)) / (Number(props.max) - Number(props.min)) + '%',
+					}}
+				/>
+			</div>
 		</>
-	)
-}
+	);
+};
 
-export default React.memo(Slider)
+export default React.memo(Slider);
