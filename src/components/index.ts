fb103d37554f2ddf8fd00d1b794e7420d2f38536
frannkenstein <deathakanote@gmail.com>
2022-11-17 15:53:05 +0530

    [index] small aditions

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import Accordion, { AccordionProps } from './Accordion/Accordion'
 import Button, { ButtonProps } from './Button/Button'
 import Label from './Label/Label'
 import Slider from './Slider/Slider'
+import DropDown, { DropDownProps } from './DropDown/DropDown'
 import '../index.scss'
 
-export { Accordion, AccordionProps, Button, ButtonProps, Label, Slider }
+export { Accordion, AccordionProps, Button, ButtonProps, Label, Slider, DropDown, DropDownProps }
