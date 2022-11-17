fb7748067cfdff18f02e2d440e12e65571d0c8ec
manis <manishdoley23@gmail.com>
2022-11-17 13:52:44 +0530

    [Divider] create divider

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Divider.scss';
+
+export interface DividerProps {
+	/**
+	 * Type of Divider
+	 * @default horizontal
+	 */
+	varient?: 'horizontal' | 'vertical'
+
+	/**
+	 * Classes to be applied.
+	 */
+	className?: string
+
+	/**
+	 * Styles to be applied.
+	 */
+	style?: React.CSSProperties
+}
+
+const Divider: React.FC<DividerProps> = (props) => {
+	return <div className={'bg-[#808080]' + (props.varient && props.varient === 'vertical' ? 'h-full w-[1px]' : 'h-[1px] w-full') + props.className} style={props.style} />
+}
+
+export default React.memo(Divider)
