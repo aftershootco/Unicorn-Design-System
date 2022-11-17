5d52953c7c43f73b8d614ba3b47fcb2fd4404b4f
frannkenstein <deathakanote@gmail.com>
2022-11-17 12:23:35 +0530

    [Button] Reskined

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,56 @@
+import React, { useMemo } from 'react'
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	/**
+	 * Text of the button
+	 */
+	text?: string
+
+	/**
+	 * Button varient.
+	 * @Default 'primary'
+	 */
+	variant?: 'primary' | 'secondary' | 'negative' | 'outline'
+
+	/**
+	 * Icon in Button
+	 */
+	suffixIcon?: JSX.Element
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+	const classesName = useMemo(() => {
+		switch (props.variant) {
+			case 'primary':
+				return 'bg-blue-500 border-transparent hover:bg-blue-300 hover:border-red-100 disabled:bg-gray-500 '
+
+			case 'secondary':
+				return 'bg-red-400 border-transparent hover:bg-red-500 hover:border-red-100 disabled:bg-gray-500 '
+
+			case 'negative':
+				return 'bg-gray-600 border-transparent hover:bg-gray-50/30 hover:border-gray-50/30 disabled:bg-gray-50 '
+
+			case 'outline':
+				return 'text-gray-50 bg-transparent border-gray-400 hover:border-gray-200 disabled:text-gray-200 '
+
+			default:
+				return ''
+		}
+	}, [props.variant])
+
+	return (
+		<button
+			className={
+				`align-center flex flex-row ${
+					props.suffixIcon ? 'justify-between' : 'justify-center'
+				} w-full cursor-pointer rounded-lg border py-3 px-5 text-slate-100 disabled:pointer-events-none disabled:cursor-default ` +
+				classesName
+			}
+			{...props}
+		>
+			<span>{props.text}</span> {props.suffixIcon}
+		</button>
+	)
+}
+
+export default React.memo(Button)
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,42 +1,7 @@
 import '../index.scss'
-// import Button, { ButtonProps } from './Button/Button'
-// import CheckBox, { CheckBoxProps } from './CheckBox/CheckBox'
-// import Chip, { ChipProps } from './Chip/Chip'
-// import Dialog, { DialogProps } from './Dialog/Dialog'
-// import Divider, { DividerProps } from './Divider/Divider'
-// import DropDown, { DropDownProps } from './DropDown/DropDown'
 import Label from './Label/Label'
 import Slider from './Slider/Slider'
-// import LinearProgressBar, { LinearProgressBarProps } from './LinearProgressBar/LinearProgressBar'
-// import Switch, { SwitchProps } from './Switch/Switch'
-// import TextInput, { TextInputProps } from './TextInput/TextInput'
-// import Toggle, { ToggleProps } from './Toggle/Toggle'
 import Accordion, { AccordionProps } from './Accordion/Accordion'
+import Button, { ButtonProps } from './Button/Button'
 
-export {
-	Label,
-	Slider,
-	// LabelProps,
-	// Button,
-	// ButtonProps,
-	// CheckBox,
-	// CheckBoxProps,
-	// Switch,
-	// SwitchProps,
-	// DropDown,
-	// DropDownProps,
-	// Dialog,
-	// DialogProps,
-	// Chip,
-	// ChipProps,
-	// TextInput,
-	// TextInputProps,
-	// Toggle,
-	// ToggleProps,
-	// Divider,
-	// DividerProps,
-	// LinearProgressBar,
-	// LinearProgressBarProps,
-	Accordion,
-	AccordionProps,
-}
+export { Label, Slider, Button, ButtonProps, Accordion, AccordionProps }
