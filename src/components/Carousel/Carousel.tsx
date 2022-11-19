47c85db14e1113ce52d4a3fefd54594758478da2
Shaney Waris <shaney18308@iiitd.ac.in>
2022-11-19 15:36:03 +0530

    [Carousal] Docs and Forward ref

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,9 +1,24 @@
 import React, { useState } from 'react'
 
 export interface CarouselProps {
+	/**
+	 * ClassName for the outer div.
+	 */
 	className?: string
+
+	/**
+	 * Styles to be aplied for the outer div.
+	 */
 	style?: React.CSSProperties
+
+	/**
+	 * Data to be shown. It should be an array of JSX.
+	 */
 	data: string[] | JSX.Element[]
+
+	/**
+	 * Default view to be shown. It should be an index of data.
+	 */
 	view?: number
 }
 
@@ -11,7 +26,7 @@ const Carousel: React.FC<CarouselProps> = React.forwardRef((props, ref: any) => 
 	const [viewIndex, setViewIndex] = useState(props.view && props.view >= 0 && props.view < props.data.length ? props.view : 0)
 
 	return (
-		<div className={'flex-col w-100 ' + props.className} style={props.style}>
+		<div {...props} className={'flex-col w-100 ' + props.className} style={props.style} ref={ref}>
 			{props.data[viewIndex] ?? 'Insufficient Data'}
 			{props.data.length > 1 && (
 				<div className='flex-row align-center justify-center m-3-t w-100'>
