55a1f5076adbfd4eaa3f044edbd768f414e47b27
manis <manishdoley23@gmail.com>
2022-11-21 17:26:41 +0530

    [Switch] changes in switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
-const Switch:React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => {
+const Switch: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => {
 	return (
-		<label className={`relative inline-block w-11 h-7 && ${props.disabled && 'bg-blue-300 opacity-20 rounded-full'}`}>
-			<input 
-                disabled={props.disabled} 
-                checked={props.checked}
-                type='checkbox' 
-                className={`opacity-0 w-0 h-0`} 
-            />
-			<span className={`absolute cursor-pointer top-0 left-0 right-0 bottom-0 bg-gray-50/10 transition-[0.4s] before:absolute before:content-[''] before:h- before:w-6 before:left- before:bottom-1 before:bg-white before:transition-[0.4s] before:rounded-[50%] rounded-3xl switch-slider`}></span>
+		<label className={`relative inline-block h-7 w-11 ${props.disabled && 'rounded-full bg-blue-300 opacity-20'}`}>
+			<input disabled={props.disabled} checked={props.checked} type='checkbox' className={`h-0 w-0 opacity-0`} />
+			<span
+				className={`absolute top-0 left-0 right-0 bottom-0 cursor-pointer rounded-3xl bg-gray-50/10 transition-[0.4s] before:absolute before:left-1 before:bottom-1 before:h-6 before:w-6 before:rounded-[50%] before:bg-white before:transition-[0.4s] before:content-[''] switch-slider`}
+			/>
 		</label>
-	);
-};
+	)
+}
 
-export default React.memo(Switch);
+export default React.memo(Switch)
