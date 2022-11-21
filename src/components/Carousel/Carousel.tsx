8bf274bee00c46f2b3a7aaa6f7937fec4aad7ce1
Shaney Waris <shaney18308@iiitd.ac.in>
2022-11-21 16:41:33 +0530

    [Carousal] New changes

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -14,34 +14,37 @@ export interface CarouselProps {
 	/**
 	 * Data to be shown. It should be an array of JSX.
 	 */
-	data: string[] | JSX.Element[]
+	children: string[] | JSX.Element[]
 
 	/**
 	 * Default view to be shown. It should be an index of data.
 	 */
-	view?: number
+	view: number
+
+	/**
+	 * Fucntion to be called on changing view.
+	 */
+	onChange: (index: number) => void
 }
 
-const Carousel: React.FC<CarouselProps> = React.forwardRef((props, ref: any) => {
-	const [viewIndex, setViewIndex] = useState(props.view && props.view >= 0 && props.view < props.data.length ? props.view : 0)
-
+const Carousel: React.FC<CarouselProps> = (props: CarouselProps) => {
 	return (
-		<div {...props} className={'flex-col w-100 ' + props.className} style={props.style} ref={ref}>
-			{props.data[viewIndex] ?? 'Insufficient Data'}
-			{props.data.length > 1 && (
+		<div className={`flex-col w-100 ${props.className}`} style={props.style}>
+			{props.children[props.view]}
+			{props.children.length > 1 && (
 				<div className='flex-row align-center justify-center m-3-t w-100'>
-					{props.data.map((_, index) => {
+					{props.children.map((_, index) => {
 						return (
 							<div
-								className={'w-3 h-3 br-50 m-2-r cursor-pointer ' + (index === viewIndex ? 'bg-white' : 'bg-43')}
-								onClick={() => setViewIndex(index)}
-							></div>
+								className={'w-3 h-3 br-50 m-2-r cursor-pointer ' + (index === props.view ? 'bg-white' : 'bg-43')}
+								onClick={() => props.onChange(index)}
+							/>
 						)
 					})}
 				</div>
 			)}
 		</div>
 	)
-})
+}
 
 export default React.memo(Carousel)
