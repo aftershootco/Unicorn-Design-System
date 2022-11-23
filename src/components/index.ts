4b5613a5af47fe7e5958d2d872a9164f9f037fd5
frannkenstein <deathakanote@gmail.com>
2022-11-23 17:04:28 +0530

    Some changes in export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,10 @@
 import '../index.scss'
-// import Accordion, { AccordionProps } from './Accordion/Accordion'
-// import Button, { ButtonProps } from './Button/Button'
-// import DropDown, { DropDownProps } from './DropDown/DropDown'
-// import Slider from './Slider/Slider'
-// import ToolTip, { ToolTipProps } from './ToolTip/ToolTip'
-// import Tabs, { TabsProps } from './Tabs/Tabs'
-import Toggle, { ToggleProps } from './Toggle/Toggle'
+import Accordion, { AccordionProps } from './Accordion/Accordion'
+import Button, { ButtonProps } from './Button/Button'
+import DropDown, { DropDownProps } from './DropDown/DropDown'
+import Slider from './Slider/Slider'
+import ToolTip, { ToolTipProps } from './ToolTip/ToolTip'
+import Tabs, { TabsProps } from './Tabs/Tabs'
 import '../index.scss'
 
-// export { Accordion, AccordionProps, Button, ButtonProps, Slider, DropDown, DropDownProps, ToolTip, ToolTipProps, Tabs, TabsProps }
-export { Toggle, ToggleProps }
+export { Accordion, AccordionProps, Button, ButtonProps, Slider, DropDown, DropDownProps, ToolTip, ToolTipProps, Tabs, TabsProps }
