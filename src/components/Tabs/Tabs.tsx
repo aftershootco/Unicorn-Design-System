9b23c93f9f2ed271cef610f87f887d3ae640b588
Yash Johri <yashjohri1200@gmail.com>
2022-11-28 11:41:41 +0530

    [Tabs] Onchange added

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,31 +1,37 @@
 import React, { useCallback, useRef } from 'react'
 
 export interface TabsProps {
-	tabsList: Array<string>
+	tabs: string[]
+	onChange: (index: number) => void
 }
 
-const Tabs: React.FC<TabsProps> = (props) => {
+const Tabs: React.FC<TabsProps> = React.memo((props) => {
 	const sliderRef = useRef(null)
-	const indicator = useCallback((e) => {
-		sliderRef.current.style.left = e.offsetLeft + 'px'
-		sliderRef.current.style.width = e.offsetWidth + 'px'
-	}, [])
+
+	const shiftIndicator = useCallback(
+		(e, index: number) => {
+			sliderRef.current.style.width = e.target.offsetWidth + 'px'
+			sliderRef.current.style.left = e.target.offsetLeft + 'px'
+			props.onChange(index)
+		},
+		[props.onChange]
+	)
 
 	return (
 		<nav className='relative flex'>
-			<div className='absolute bottom-5 h-1 rounded-lg bg-gray-50 duration-300' ref={sliderRef} style={{ left: '0.75rem', width: '8rem' }} />
-			{props.tabsList.map((item) => {
+			<div className='absolute bottom-5 left-3 h-1 w-32 rounded-lg bg-gray-50 duration-300' ref={sliderRef} />
+			{props.tabs.map((tab, index) => {
 				return (
 					<div
 						className='relative my-0 mx-3 flex min-w-8 cursor-pointer justify-center text-2xl text-gray-50'
-						onClick={(e) => indicator(e.target)}
+						onClick={(e) => shiftIndicator(e, index)}
 					>
-						{item}
+						{tab}
 					</div>
 				)
 			})}
 		</nav>
 	)
-}
+})
 
-export default React.memo(Tabs)
+export default Tabs
