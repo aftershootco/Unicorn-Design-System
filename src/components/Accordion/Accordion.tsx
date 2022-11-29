ac2e0849a0209e7c83d9b269f3cc0d88201e5c7b
frannkenstein <deathakanote@gmail.com>
2022-11-29 12:40:50 +0530

    [Accordin] Some changes

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,5 +1,5 @@
 import { ArrowIcon } from '@aftershootco/unicorn-icons'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef } from 'react'
 
 export interface AccordionProps {
 	expanded?: boolean
@@ -12,7 +12,7 @@ export interface AccordionProps {
 	headerClassName?: string
 	headerStyle?: React.CSSProperties
 
-	children: string | JSX.Element
+	children: JSX.Element
 	bodyClassName?: string
 	bodyStyle?: React.CSSProperties
 
@@ -20,20 +20,15 @@ export interface AccordionProps {
 }
 
 const Accordion: React.FC<AccordionProps> = (props) => {
-	const [height, setHeight] = useState('0')
 	const contentSpace = useRef(null)
 
-	useEffect(() => {
-		setHeight(props.expanded && `${contentSpace.current.scrollHeight}`)
-	}, [props.children])
-
-	function toggleAccordion(e) {
+	const toggleAccordion = (e) => {
 		e.stopPropagation()
 		props.onClick(e)
-		setHeight(props.expanded ? '0' : `${contentSpace.current.scrollHeight}`)
 	}
+
 	return (
-		<div className='mb-2 w-full cursor-pointer rounded bg-gray-800 py-3 px-4 text-lg'>
+		<div className='w-full cursor-pointer rounded bg-gray-800 text-lg mb-2 py-3 px-4'>
 			<div className='flex w-full items-center justify-between' onClick={toggleAccordion}>
 				<div className='flex items-center'>
 					<ArrowIcon className={`text-gray-400 transition-all duration-300 ${props.expanded ? 'rotate-90' : 'rotate-0'}`} />
@@ -48,13 +43,13 @@ const Accordion: React.FC<AccordionProps> = (props) => {
 			<div
 				style={{
 					minHeight: '0px',
-					height: props.expanded ? `${height}px` : '0px',
+					height: props.expanded ? 'auto' : '0px',
 					opacity: props.expanded ? 1 : 0,
 					transition: '300ms',
 					overflow: props.expanded ? 'visible' : 'hidden',
 				}}
 				ref={contentSpace}
-				className={`px-4 ${props.expanded ? 'my-2' : 'py-0'} transition-height w-full text-base text-gray-200 duration-300 ease-in-out ${
+				className={`px-4 ${props.expanded ? 'my-2' : 'py-0'} w-full text-base text-gray-200 transition-height duration-300 ease-in-out ${
 					props.bodyClassName
 				}`}
 			>
