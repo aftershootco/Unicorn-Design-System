26b23f2ab33fec95210054ed5a983f7176a641d7
manis <manishdoley23@gmail.com>
2022-12-02 11:47:52 +0530

    [Slider] update slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,26 +1,25 @@
-import clsx from 'clsx'
 import React from 'react'
 import './Slider.scss'
 
-const Slider: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = React.memo((props) => {
+const Slider: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = (props) => {
 	return (
-		<div className='flex h-8 w-full items-center justify-center rounded'>
-			<input
-				{...props}
-				className={clsx(
-					'h-8 w-4/5 cursor-pointer rounded-sm',
-					'slider-input bg-gray-50 bg-gradient-to-b bg-no-repeat',
-					!props.disabled ? 'from-blue-600 to-blue-600' : 'from-gray-50 to-gray-50 opacity-40',
-					props.className
-				)}
-				type='range'
-				style={{
-					...props.style,
-					backgroundSize: ((Number(props.value) - Number(props.min)) * Number(props.max)) / (Number(props.max) - Number(props.min)) + '%',
-				}}
-			/>
-		</div>
+		<>
+			<div className={`flex h-[34px] w-full items-center justify-center rounded`}>
+				<input
+					{...props}
+					disabled={props.disabled}
+					className={`w-4/5 rounded-sm bg-gradient-to-b ${
+						!props.disabled ? 'from-[#2279CE] to-[#2279CE]' : 'from-[#e2e2e2] to-[#e2e2e2] opacity-40'
+					} slider-input bg-[#E2E2E21A] bg-no-repeat ${props.className}`}
+					type='range'
+					style={{
+						...props.style,
+						backgroundSize: ((80 - 10) * 100) / (100 - 0) + '%',
+					}}
+				/>
+			</div>
+		</>
 	)
-})
+}
 
 export default Slider
