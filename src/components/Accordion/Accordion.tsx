99bfe53ac9303c892f12b2fd56f97aa8907a2ddc
Yash Johri <yashjohri1200@gmail.com>
2022-12-03 20:12:02 +0530

    [Acordion] Fix

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -40,7 +40,7 @@ const Accordion: React.FC<AccordionProps> = React.memo((props) => {
 				ref={contentSpace}
 				className={clsx(
 					'transition-height min-h-0 w-full px-4 text-base text-gray-200 duration-300 ease-in-out',
-					props.expanded ? 'my-2 h-auto overflow-visible opacity-0' : 'h-0 overflow-hidden py-0 opacity-100'
+					props.expanded ? 'my-2 h-auto overflow-visible opacity-100' : 'h-0 overflow-hidden py-0 opacity-0'
 				)}
 			>
 				{props.children}
