16bdd5dd864c892103d93b338247996cfa3bb716
Yash Johri <yashjohri1200@gmail.com>
2023-01-11 19:35:57 +0530

    [Chip] Add forward ref

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -18,22 +18,25 @@ export interface ChipProps extends React.ButtonHTMLAttributes<HTMLButtonElement>
 	selectedClassName?: string
 }
 
-const Chip: React.FC<ChipProps> = React.memo((props) => {
-	return (
-		<button
-			{...props}
-			className={clsx(
-				'rounded-lg border border-gray-50/25 py-1 px-3 text-lg text-gray-50',
-				'transition-all duration-300',
-				!props.disabled ? 'hover:border-gray-50/100' : 'text-gray-50/30',
-				props.selected && !props.disabled && 'border-blue-400 bg-blue-400 hover:border-blue-400',
-				props.className,
-				props.selected && props.selectedClassName
-			)}
-		>
-			{props.text || props.children}
-		</button>
-	)
-})
+const Chip: React.FC<ChipProps> = React.forwardRef(
+	React.memo((props, ref) => {
+		return (
+			<button
+				{...props}
+				ref={ref}
+				className={clsx(
+					'rounded-lg border border-gray-50/25 py-1 px-3 text-lg text-gray-50',
+					'transition-all duration-300',
+					!props.disabled ? 'hover:border-gray-50/100' : 'text-gray-50/30',
+					props.selected && !props.disabled && 'border-blue-400 bg-blue-400 hover:border-blue-400',
+					props.className,
+					props.selected && props.selectedClassName
+				)}
+			>
+				{props.text || props.children}
+			</button>
+		)
+	})
+)
 
 export default Chip
