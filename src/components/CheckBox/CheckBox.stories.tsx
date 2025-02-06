baa8b3c1f87696f2109be3c8b4d36456460ce4c9
Booi <booimangang@gmail.com>
2025-02-06 14:57:09 +0530

    update: checkbox component (#59)
    * update: checkbox component
    
    * update: create type of checkbox variant
    
    * update: use enum
    
    * update: fix size
    
    * update: util index

--- src/components/CheckBox/CheckBox.stories.tsx
+++ src/components/CheckBox/CheckBox.stories.tsx
@@ -13,7 +13,45 @@ const Default = Template.bind({})
 Default.args = {
 	value: false,
 	disabled: false,
-	varient: 'primary',
+	variant: 'primary',
 }
 
-export { Default }
+const DefaultActive = Template.bind({})
+DefaultActive.args = {
+	value: true,
+	disabled: false,
+	variant: 'primary',
+}
+
+const DefaultActiveSm = Template.bind({})
+DefaultActiveSm.args = {
+	value: true,
+	size: 'sm',
+	disabled: false,
+	variant: 'primary',
+}
+
+const DefaultActiveMd = Template.bind({})
+DefaultActiveMd.args = {
+	value: true,
+	size: 'md',
+	disabled: false,
+	variant: 'primary',
+}
+
+const DefaultActiveLg = Template.bind({})
+DefaultActiveLg.args = {
+	value: true,
+	size: 'lg',
+	disabled: false,
+	variant: 'primary',
+}
+
+const Secondary = Template.bind({})
+Secondary.args = {
+	value: true,
+	disabled: false,
+	variant: 'secondary',
+}
+
+export { Default, DefaultActive, DefaultActiveLg, DefaultActiveMd, DefaultActiveSm, Secondary }
--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,6 +1,16 @@
-import { CheckIcon, TickIcon } from '@aftershootco/unicorn-icons'
+import { TickIcon } from '@aftershootco/unicorn-icons'
 import clsx from 'clsx'
 import React, { useCallback } from 'react'
+import { cn } from '../../utils'
+
+export enum CheckboxVariant {
+	Small = 'sm',
+	Medium = 'md',
+	Large = 'lg',
+}
+
+export type TCheckboxVariant = `${CheckboxVariant}`
+
 export interface CheckBoxProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
 	/**
 	 *
@@ -21,6 +31,12 @@ export interface CheckBoxProps extends Omit<React.HTMLAttributes<HTMLDivElement>
 	 * color of a checkbox
 	 */
 	color?: string
+
+	/**
+	 * pre defined size of a checkbox
+	 */
+
+	size?: TCheckboxVariant
 
 	/**
 	 * Value of the checkbox.
@@ -49,25 +65,34 @@ const CheckBox: React.FC<CheckBoxProps> = React.memo((props) => {
 	)
 
 	return (
-		<div id={props.id}>
+		<div id={props.id} className='group w-fit'>
 			{props.variant === 'primary' && (
 				<div
-					className={clsx(
-						'w-fit rounded-sm border border-solid border-gray-400 p-0.5',
-						props.disabled ? 'pointer-events-none cursor-default bg-transparent' : 'cursor-pointer bg-gray-50/30',
+					className={cn(
+						'grid w-max cursor-pointer place-items-center rounded-[0.188rem] border border-solid border-gray-50/10 bg-gray-50/10 p-[0.156rem] transition-all duration-200 ease-in-out  group-hover:border-gray-400',
+						props.disabled && 'pointer-events-none cursor-default bg-transparent',
 						props.className
 					)}
 					onClick={onClick}
 				>
 					{props.value ? (
 						<CheckIcon
-							className={clsx(
-								'pointer-events-auto h-5 w-5 rounded-sm transition-all duration-300	',
-								props.disabled ? 'bg-gray-50/30' : 'bg-blue-400 hover:bg-blue-500'
+							className={cn(
+								'pointer-events-auto h-[0.844rem] w-[0.844rem] rounded-[0.063rem] bg-blue-400 transition-all duration-200 ease-in-out  group-hover:bg-blue-300',
+								props.disabled && 'bg-gray-50/30',
+								// padding around the check icon is 2.5px
+								props.size === CheckboxVariant.Small && 'h-[0.719rem] w-[0.719rem]', // 14px - 2.5px
+								props.size === CheckboxVariant.Large && 'h-[1.094rem] w-[1.094rem]' //20px - 2.5px
+								// size md is default - 16px - 2.5px
 							)}
 						/>
 					) : (
-						<div className={clsx('h-5 w-5 rounded-sm transition-all duration-300', !props.disabled && 'hover:bg-gray-50/30')} />
+						<div
+							className={cn(
+								'h-[0.844rem] w-[0.844rem] rounded-[0.063rem] transition-all duration-200 ease-in-out',
+								!props.disabled && 'group-hover:bg-gray-50/20'
+							)}
+						/>
 					)}
 				</div>
 			)}
@@ -109,3 +134,16 @@ CheckBox.defaultProps = {
 }
 
 export default CheckBox
+
+const CheckIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
+	return (
+		<svg width='12' height='12' viewBox='0 0 12 12' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+			<path
+				fill-rule='evenodd'
+				clip-rule='evenodd'
+				d='M8.81598 3.40349C9.01479 3.53349 9.0706 3.80004 8.94061 3.99885L6.01589 8.47195C5.94697 8.57738 5.8353 8.64723 5.71037 8.66319C5.58543 8.67916 5.45979 8.63952 5.36659 8.55481L3.47413 6.8344C3.29837 6.67461 3.28541 6.40259 3.4452 6.22683C3.60498 6.05106 3.877 6.0381 4.05277 6.1979L5.57198 7.57899L8.22065 3.52811C8.35064 3.3293 8.61717 3.27351 8.81598 3.40349Z'
+				fill='#E2E2E2'
+			/>
+		</svg>
+	)
+}
