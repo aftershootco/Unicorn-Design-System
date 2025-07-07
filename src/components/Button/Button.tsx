808aa5085304d0b865073dea1f3cf5e83e6892ea
Booi <booimangang@gmail.com>
2025-07-07 16:53:30 +0530

    update: able to pass ref to Button (#64)
    * update: able to pass ref to Button
    
    * update: add react memeo to Button
    
    * fix: package version
    
    * update: move memo while exporting
    
    * update: fix version

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -33,7 +33,7 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 	disableduringcallback?: boolean
 }
 
-const Button: React.FC<ButtonProps> = React.memo((props) => {
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
 	const { disableduringcallback, ...rest } = props
 	const [isDisabled, setDisabled] = useState(false)
 
@@ -71,6 +71,7 @@ const Button: React.FC<ButtonProps> = React.memo((props) => {
 	return (
 		<button
 			{...rest}
+			ref={ref}
 			onClick={handleOnClick}
 			disabled={isDisabled || props.disabled}
 			className={clsx(
@@ -88,4 +89,6 @@ const Button: React.FC<ButtonProps> = React.memo((props) => {
 	)
 })
 
-export default Button
+Button.displayName = 'Button'
+
+export default React.memo(Button)
